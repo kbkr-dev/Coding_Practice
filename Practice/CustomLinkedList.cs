dcef90ab55a5af34cd45a076c0a658e0c634b7de
Bharath kumar reddy kummetha <kbkr033@gmail.com>
2024-11-26 22:30:11 +0530

    linkedlist

---
+++ Practice/CustomLinkedList.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Practice.Practice
+{
+    public class Node<TValue>
+    {
+        public TValue Data;
+        public Node<TValue> Next;
+
+        public Node(TValue data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+    public class CustomLinkedList<TValue>
+    {
+        private Node<TValue> head;
+        private Node<TValue> tail;
+        private int length;
+        public CustomLinkedList(TValue value)
+        {
+            head = new Node<TValue>(value);
+            tail = head;
+            length = 1;
+        }
+
+        public void append(TValue value)
+        {
+            var node = new Node<TValue>(value);
+            tail.Next = node;
+            tail = node;
+            length++;
+        }
+
+        public void prepend(TValue value)
+        {
+            var node = new Node<TValue>(value);
+            node.Next = head;
+            head = node;
+            length++;
+        }
+
+        public void insert(int index, TValue value)
+        {
+            index = WrapIndex(index);
+
+            if (index == 0)
+            {
+                prepend(value);
+                return;
+            }
+
+            if (index == length - 1)
+            {
+                append(value);
+                return;
+            }
+
+            var node = new Node<TValue>(value);
+            var leader = TraverseToIndex(index);
+            var holdingPointer = leader.Next;
+
+            leader.Next = node;
+            node.Next = holdingPointer;
+
+            length++;
+        }
+
+        public void remove(int index)
+        {
+            index = WrapIndex(index);
+
+            if (index == 0)
+            {
+                head = head.Next;
+                return;
+            }
+
+            var leader = TraverseToIndex(index);
+            var removePointer = leader.Next;
+            leader.Next = removePointer.Next;
+            length--;
+        }
+
+        public void reverse()
+        {
+            var first = head;
+            tail = head;
+
+            var second = first.Next;
+
+            for (int i = 0; i < length - 1; i++)
+            {
+                var temp = second.Next;
+                second.Next = first;
+                first = second;
+                second = temp;
+            }
+
+            head.Next = null;
+            head = first;
+        }
+
+
+        private int WrapIndex(int index)
+        {
+            return Math.Max(Math.Min(index, length - 1), 0);
+        }
+
+        private Node<TValue> TraverseToIndex(int index)
+        {
+            int counter = 0;
+
+            index = WrapIndex(index);
+            var currentNode = head;
+
+            while (counter != index)
+            {
+                currentNode = currentNode.Next;
+                counter++;
+            }
+
+            return currentNode;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using BenchmarkDotNet.Running;
 using Coding_Practice.Arrays;
 using Coding_Practice.Practice;
+using System.Collections.Generic;
 
 namespace Coding_Practice
 {
@@ -24,19 +25,29 @@ namespace Coding_Practice
             //var result = LongestWord.LongestWordIn("This is most@#$% of the funny time and good");
             //Console.WriteLine(result);
 
-            HashTable<string, string> hash = new HashTable<string, string>();
+            //HashTable<string, string> hash = new HashTable<string, string>();
 
-            hash.AddData("key1", "value1");
-            hash.AddData("key2", "value2");
-            hash.AddData("key3", "value3");
-            hash.AddData("key4", "value4");
+            //hash.AddData("key1", "value1");
+            //hash.AddData("key2", "value2");
+            //hash.AddData("key3", "value3");
+            //hash.AddData("key4", "value4");
 
-            if (hash.TryGetValue("key1", out var key))
-            {
-                Console.WriteLine(key);
-            }
+            //if (hash.TryGetValue("key1", out var key))
+            //{
+            //    Console.WriteLine(key);
+            //}
 
-            hash.Remove("key2");
+            //hash.Remove("key2");
+
+
+            CustomLinkedList<int> l = new CustomLinkedList<int>(10);
+            l.append(5);
+            l.append(16);
+            l.prepend(1);
+            l.insert(2, 99);
+            l.insert(20, 7);
+            l.remove(2);
+            l.reverse();
 
             Console.ReadKey();
         }
